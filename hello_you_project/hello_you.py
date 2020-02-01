c21a9008644474cba2517d3cdc6eaa2d83c35973
Mathieu Stark <mathieu@analytiks.co>
2020-02-01 12:37:34 -0800

    files changes

---
+++ hello_you_project/hello_you.py
@@ -0,0 +1,16 @@
+#Ask User for Name
+name = input('What is your name? ')
+
+#Ask User for Age
+age = input('How old are you? ')
+
+#Ask User for City
+city = input('What city do you live in? ')
+
+#Ask User what they Enjoy
+enjoy = input('What activities do you enjoy? ')
+
+#Create Output text
+print('My name is ' + name + ' and I am ' + age + ' years old.')
+
+#Print Output to screen
